825636cea746d038261642eb2b285ad18958f539
djayfresh <douglasjayfresh@gmail.com>
2020-03-26 21:13:34 -0500

    Adding some deploy tools

---
+++ tools/build.js
@@ -0,0 +1,11 @@
+({
+    "baseUrl": "../scripts",
+    "mainConfigFile": "../scripts/app.js",
+    "dir": "../dist",
+    "modules": [
+        {
+            "name": "app"
+        }
+    ],
+    "optimize": 'none'
+})
