cf779963d02cfd4585b445a04efef2d50810c6fc
Doug Fresh <douglasjayfresh@gmail.com>
2020-03-29 22:31:47 -0500

    Remove console log

--- scripts/shared/game.ts
+++ scripts/shared/game.ts
@@ -86,7 +86,6 @@ export class Game {
             });
     
             const onReset = debounce(() => {
-                console.log("Restart", this);
                 this.Restart();
             }, 500, true);
             new Key(KEY_CONST.r).onClick(onReset);
